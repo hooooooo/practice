00fabd98a2e5b7a743fc06261f2f36df2f1abe66
Zero <zerochou@outlook.com>
2021-12-12 14:52:30 +0800

    Create 416.java

---
+++ 416.java
@@ -0,0 +1,28 @@
+class Solution {
+    
+    public boolean canPartition(int[] nums) {
+        int sum  = 0;
+        for(int n : nums){
+            sum += n;
+        }
+        
+        if(sum %2 == 1){
+            return false;
+        }
+        
+        sum /= 2;
+        
+        boolean [][]dp = new boolean[nums.length+1][sum+1];
+        dp[0][0] = true;
+        
+        for(int i = 1; i<=nums.length; i++){
+            for(int j = 1; j<=sum; j++){
+                dp[i][j] = dp[i-1][j];
+                if(!dp[i][j] && j >= nums[i-1]){
+                    dp[i][j] = dp[i-1][j - nums[i-1]];
+                }
+            }
+        }
+        return dp[nums.length][sum];
+    }
+}
